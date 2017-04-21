1e1246461ad92113f908287493646022ee4eb0c0
Brian <broldak@gmail.com>
2017-04-21 16:19:32 -0700

    add signup ui

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,20 @@ class UsersController < ApplicationController
   end
 
   # POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to controller: 'users', action: 'show', id: @user.id
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  # POST /users
   # POST /users.json
   # def create
   #   @user = User.new(user_params)
@@ -54,6 +68,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email)
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   get '/signup', to: 'users#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/signup',  to: 'users#create'
+  get '/profile/:id', to: 'users#show'
 end
