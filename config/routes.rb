dd232c5991620cfa451ddeb103564a30c6a2bf02
Brian <broldak@gmail.com>
2017-04-30 21:13:38 -0700

    add test for user creation + redirect

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new'
   post '/signup',  to: 'users#create'
-  get '/profile/:id', to: 'users#show'
+  get '/profile/:id', to: 'users#show', as: 'profile'
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -10,11 +10,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  # test "should create user" do
-  #   assert_difference('User.count') do
-  #     post users_url, params: { user: { email: @user.email, first_name: @user.first_name, last_name: @user.last_name } }
-  #   end
-  #
-  #   assert_redirected_to user_url(User.last)
-  # end
+  test "should create user" do
+    assert_difference('User.count') do
+      post signup_url, params: { user: { email: 'broldak124490@gmail.com', first_name: @user.first_name, last_name: @user.last_name, password: 'password', password_confirmation: 'password' } }
+    end
+
+    assert_redirected_to profile_url(User.last)
+  end
 end
